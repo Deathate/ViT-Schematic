d12b2b2f82762ccc0a00260c4b2f29177f23e0a4
deathate <ho.aron0105@gmail.com>
2024-09-23 15:39:50 +0800

    Auto commit 15/39/09/23/2024

--- gungunexample.py
+++ gungunexample.py
@@ -1,6 +1,6 @@
 from test_script.test_slice_image_line import analyze_connection
 from utility import *
 if __name__ == "__main__":
-    path = "dataset_fullimg_mask/images/circuit50038.png"
+    path = "test_images/circuit50038.png"
     group_connection, img = analyze_connection(cv2.imread(path, cv2.IMREAD_UNCHANGED), debug=False)
     cv2.imwrite("tmp.png", img)
