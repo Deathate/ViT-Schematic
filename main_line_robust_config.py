34d934be6be412166052fafe9041ddd1d3e1a70f
deathate <ho.aron0105@gmail.com>
2024-12-04 11:32:20 +0800

    Auto commit 11/32/12/04/2024

--- main_line_robust_config.py
+++ main_line_robust_config.py
@@ -18,7 +18,6 @@ STYLE_OPTIONS = ["encoder, decoder", "cnn"]
 MODEL_STYLE = STYLE_OPTIONS[1]
 REAL_DATA = False
 SMALL_IMAGE = True
-ZOOM = False
 CLASS_OUTPUT = False
 UPLOAD = False
 
@@ -28,40 +27,18 @@ class DatasetConfig(Enum):
     REAL = auto()
 
 
-# weight version
-WEIGHT_VERSION = {
-    1: "runs/FormalDatasetWindowedLinePair/1027_20-00-43/best.pth",  # real data, 12 direction, cnn, 100 pick, IMAGE_SIZE = 50
-    2: "runs/FormalDatasetWindowedLinePair/1030_11-02-14/best.pth",  # real data, 1 direction, cnn, 300 pick, IMAGE_SIZE = 50, pretrained from 1
-    7: "runs/FormalDatasetWindowedLinePair/1105_14-34-32/best.pth",  # real data, 1 direction, cnn, 300 pick, IMAGE_SIZE = 50, pretrained from 2
-    8: "runs/FormalDatasetWindowedLinePair/1107_11-15-46/best.pth",  # real data, 4 direction, cnn, 300 pick, IMAGE_SIZE = 50, no pretrained
-    9: "runs/FormalDatasetWindowedLinePair/1107_23-19-44/best.pth",  # real data, 4 direction, cnn, 150 pick, IMAGE_SIZE = 100, no pretrained
-    10: "runs/FormalDatasetWindowedLinePair/1108_23-28-29/best.pth",  # real data, 4 direction, cnn, 1 pick, IMAGE_SIZE = FULL, no pretrained
-    11: "runs/FormalDatasetWindowedLinePair/1112_00-59-53/best.pth",  # cc data, 1 direction, cnn, 1 pick, IMAGE_SIZE = FULL, no pretrained
-}
-
-
-def get_best_model_path(config, full_size, class_output=False):
+def get_best_model_path(config, class_output=False):
     global IMAGE_SIZE, RESULT_NUM, CLASS_OUTPUT
     if config == DatasetConfig.CC:
-        if not full_size:
-            RESULT_NUM = 35
-            IMAGE_SIZE = 50
-            return "runs/FormalDatasetWindowedLinePair/1012_02-08-46/best.pth"
-        else:
-            RESULT_NUM = 125
-            IMAGE_SIZE = -1
-            return WEIGHT_VERSION[11]
+        RESULT_NUM = 35
+        IMAGE_SIZE = 50
+        return "runs/FormalDatasetWindowedLinePair/1123_18-10-31/best_train.pth"
     elif config == DatasetConfig.REAL:
         if class_output:
             CLASS_OUTPUT = True
-            if not full_size:
-                RESULT_NUM = 10
-                IMAGE_SIZE = 50
-                return "runs/FormalDatasetWindowedLinePair/1118_17-28-26/best_train.pth"
-            else:
-                RESULT_NUM = 125
-                IMAGE_SIZE = -1
-                return WEIGHT_VERSION[10]
+            RESULT_NUM = 10
+            IMAGE_SIZE = 50
+            return "runs/FormalDatasetWindowedLinePair/1118_17-28-26/best_train.pth"
         else:
             CLASS_OUTPUT = False
             RESULT_NUM = 10
@@ -116,7 +93,6 @@ if MODEL_STYLE == STYLE_OPTIONS[1]:
         TEST_DATASET_PATH = "cc_deathate_data/train"
         DIRECTION = 1
         if REAL_DATA:
-            PRETRAINED_PATH = WEIGHT_VERSION[10]
             RESULT_NUM = 125
             DATASET_PATH = "real_data/train"
             EVAL_DATASET_PATH = None
